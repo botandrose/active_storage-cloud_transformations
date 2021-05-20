d5a04212a7dcb179f9b7e3a561b84e32eeab8b44
Micah Geisel <micah@botandrose.com>
2021-05-20 10:48:58 -0700

    provide path for our previews, too.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resolve("ActiveStorage::CloudTransformations::Variant") { |variant, options| route_for(ActiveStorage.resolve_model_to_route, variant, options) }
+  resolve("ActiveStorage::CloudTransformations::Preview") { |preview, options| route_for(ActiveStorage.resolve_model_to_route, preview, options) }
 end
 
