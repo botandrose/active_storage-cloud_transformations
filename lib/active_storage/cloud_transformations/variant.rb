9422e53fd43971042a29b9309883bf40b6138e48
Micah Geisel <micah@botandrose.com>
2021-06-26 15:06:05 -0700

    disable video variants and swap out transloadit for crucible for image variants.

--- lib/active_storage/cloud_transformations/variant.rb
+++ lib/active_storage/cloud_transformations/variant.rb
@@ -4,7 +4,9 @@ require "transloadit"
 module ActiveStorage
   module CloudTransformations
     class Variant < ActiveStorage::VariantWithRecord
-      def process wait: true
+      def process
+        raise ActiveStorage::InvariableError unless blob.image?
+
         ActiveRecord::Base.connected_to(role: ActiveRecord::Base.writing_role) do
           # FIXME #create_or_find_by! runs the block in both cases. bug in rails?
           blob.variant_records.find_or_create_by!(variation_digest: variation.digest) do |record|
@@ -16,7 +18,7 @@ module ActiveStorage
               checksum: 0, # we don"t know this yet, can we get it from the results?
             })
             record.image.attach(output_blob)
-            start_transloadit_job(blob, output_blob, wait: wait)
+            run_crucible_job(blob, output_blob)
           end
         end
       rescue ActiveRecord::RecordNotUnique
@@ -25,73 +27,27 @@ module ActiveStorage
 
       private
 
-      def start_transloadit_job input_blob, output_blob, wait:
-        import = transloadit.step "import", "/s3/import",
-          key: s3_credentials[:access_key_id],
-          secret: s3_credentials[:secret_access_key],
-          bucket: s3_credentials[:bucket],
-          bucket_region: s3_credentials[:region],
-          path: blob.key
-
+      def run_crucible_job input_blob, output_blob
         width, height = variation.transformations.fetch(:resize_to_limit)
-        resize = transloadit.step "resize", resize_step,
-          width: width,
-          height: height,
-          format: format,
-          **resize_options
-
-        store = transloadit.step "store", "/s3/store",
-          key: s3_credentials[:access_key_id],
-          secret: s3_credentials[:secret_access_key],
-          bucket: s3_credentials[:bucket],
-          bucket_region: s3_credentials[:region],
-          path: output_blob.key
-        assembly = transloadit.assembly(steps: [import, resize, store])
-
-        response = assembly.create!
-        return true unless wait
-
-        response.reload_until_finished!
-        !response.error? || (raise response.to_s)
+        post! "https://huuabwxpqf.execute-api.us-west-2.amazonaws.com/prod/image/variant", {
+          blob_url: input_blob.url.split("?").first,
+          dimensions: "#{width}x#{height}",
+          image_variant_url: output_blob.url.split("?").first,
+        }
       end
 
       def format
         variation.transformations.fetch(:format)
       end
 
-      def resize_step
-        return "/video/encode" if blob.video?
-        return "/image/resize" if blob.image?
-        raise ActiveStorage::InvariableError
-      end
-
-      def resize_options
-        if blob.video?
-          {
-            ffmpeg_stack: "v4.3.1",
-            preset: "hls-720p",
-            resize_strategy: "fit",
-            turbo: false,
-          }
-        elsif blob.image?
-          {
-            quality: variation.transformations.fetch(:quality, 92),
-            colorspace: "sRGB",
-          }
-        else raise ActiveStorage::InvariableError
-        end
-      end
-
-      def transloadit
-        @transloadit ||= Transloadit.new(service_credentials(:transloadit))
-      end
-
-      def s3_credentials
-        @s3_credentials ||= service_credentials(ActiveStorage::Blob.service.name)
-      end
-
-      def service_credentials key
-        ::Rails.configuration.active_storage.service_configurations[key.to_s].symbolize_keys
+      def post! url, body
+        uri = URI.parse(url)
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = uri.scheme == "https"
+        request = Net::HTTP::Post.new(uri.request_uri, {"Content-Type": "application/json"})
+        request.body = body.to_json
+        response = http.request(request)
+        response.code == "201" || (raise body.to_json + response.inspect)
       end
     end
   end
